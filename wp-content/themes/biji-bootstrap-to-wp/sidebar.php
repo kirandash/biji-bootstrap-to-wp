6bb0279b8b7aa7a060e45b3002bb7cbc2037ea1d
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-08 08:21:59 +0530

    Add default index.php.Add front-page layout.Add Grid layout full and with sidebar pages.

---
+++ wp-content/themes/biji-bootstrap-to-wp/sidebar.php
@@ -0,0 +1,6 @@
+<div class="col-md-3 sidebar">
+  <?php if(!dynamic_sidebar('page'))?>
+  
+  <h3>Sidebar setup</h3>
+  <p>Please add widgets to the page sidebar to have them display here.</p>
+</div>
